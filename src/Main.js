84b1e1a791b80efc54aa002df126e85e58f1b069
robigan <sebastian.colombo06@gmail.com>
2021-03-25 12:52:21 +0100

    Yea, f*** the extra formatting file

--- src/Main.js
+++ src/Main.js
@@ -1,50 +1,56 @@
 const { Console } = console;
-const { PromptsCompat } = require("./Formating.js");
 
-const assertOptions = (Options) => {
-    if (!Options && typeof Options === "object") {
-        throw new TypeError("Options cannot be falsy and must be an object");
-    } else if (!Options.std.stdout) {
-        throw new TypeError("Options.std.stdout cannot be falsy");
+const assertTerms = (Terms) => {
+    if (!Terms && typeof Terms === "object") {
+        throw new TypeError("Terms cannot be falsy and must be an object");
+    } else if (!Terms.std.stdout) {
+        throw new TypeError("Terms.std.stdout cannot be falsy");
     }
-    Options.others.forEach((element, index) => {
+    Terms.others.forEach((element, index) => {
         if (!element.stdout) {
-            throw new TypeError(`stdOut for Options.others[${index}] must be specified`);
+            throw new TypeError(`stdOut for Terms.others[${index}] must be specified`);
         }
     });
 };
 
 class Main {
-    constructor(Options = {
+    constructor(Terms = {
         std: {
-            stdout: process.stdout, 
+            stdout: process.stdout,
             stderr: process.stderr,
             enabled: true,
         },
         others: [
             {
-                stdout: require("fs").createWriteStream(`./logs/${Date.now()}.log`, {flags: "wx"}),
+                stdout: require("fs").createWriteStream(`./logs/${Date.now()}.log`, { flags: "wx" }),
                 name: "fs",
                 enabled: true,
             },
         ],
-    }) {
-        assertOptions(Options);
-        //Options.std ? super(Options.std) : super(process.stdout);
+    }, Options = new Map([["Kleur", true], ["Prompts", true]])) {
+        assertTerms(Terms);
+        //Terms.std ? super(Terms.std) : super(process.stdout);
 
-        //this.Options = Options;
-        this.Console = new Console(Options.std);
+        //this.Terms = Terms;
+        this.Console = new Console(Terms.std);
         this.Consoles = new Map();
-        this.Prompts = new PromptsCompat(Options.std);
 
-        this.Consoles.set(Options.std.name ?? "std", this.Console);
-        Options.others.forEach((element, index) => {
+        this.Consoles.set(Terms.std.name ?? "std", this.Console);
+        Terms.others.forEach((element, index) => {
             if (element.name && this.Consoles.has(element.name)) {
                 throw new SyntaxError("Cannot instanitiate a new console with a name thats already registered");
             } else if (element.enabled) {
                 this.Consoles.set(element.name ?? index, new Console(element));
             }
         });
+
+        if (Options.get("Prompts")) {
+            this.Prompts = require("prompts");
+            this.Prompt = async (Questions, Options) => {
+                return await this.Prompts(Object.assign(Terms.std, Questions), Options);
+            };
+        }
+        Options.get("Kleur") ? this.Kleur = require("kleur") : undefined;
     }
 
     async runOnAll(toRunFunc) {
@@ -66,7 +72,7 @@ class Main {
             element.clear();
         });
     }
-    
+
     async debug(...Message) {
         this.Console.log(...Message);
     }
